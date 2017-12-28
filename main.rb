64bc98d1cd6da63cebc4c4ca0e9478ec139cf6d9
Yuta Igarashi <y.iga.thunder@gmail.com>
2017-12-28 17:47:28 +0900

    fix

--- main.rb
+++ main.rb
@@ -9,13 +9,14 @@ start_num = ARGV[1]
 end_num   = ARGV[2]
 
 (start_num..end_num).to_a.each do |num|
-  content = format('%s%05d', capital, num)
+  content_label = format('%s%05d', capital, num)
+  content = content_label.to_i(16)
 
   # QRコード生成
   code128 = Barby::Code128B.new(content)
 
   # PNGで出力
-  png_file = "#{content}.png"
+  png_file = "#{content_label}.png"
   File.open("outputs/#{png_file}", 'wb') do |f|
     f.write code128.to_png(xdim: xdim)
   end
