907551770bda12186ed6fe9ab2bdf04f68202153
Yuta Igarashi <y.iga.thunder@gmail.com>
2017-12-28 17:25:13 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem 'barby'
+gem 'rqrcode'
+gem 'chunky_png'
+
+# gem "rails"
---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'barby/barcode/code_128'
+require 'barby/outputter/png_outputter'
+
+# パラメータ
+xdim    = 3  # 一番細いバーの幅
+
+capital   = ARGV[0]
+start_num = ARGV[1]
+end_num   = ARGV[2]
+
+(start_num..end_num).to_a.each do |num|
+  content = format('%s%05d', capital, num)
+
+  # QRコード生成
+  code128 = Barby::Code128B.new(content)
+
+  # PNGで出力
+  png_file = "#{content}.png"
+  File.open("outputs/#{png_file}", 'wb') do |f|
+    f.write code128.to_png(xdim: xdim)
+  end
+
+  # HTMLのimgタグ用のbase64で出力
+  # puts qrcode.to_image(xdim: xdim).to_data_url
+end
